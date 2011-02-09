a64a977866a61380801b5e3162eb5ee4babe8b0c
Dan Hansen <mokker1234@gmail.com>
2011-02-09 01:07:02 -0600

    basic testing, fix factory method

---
+++ test/test-i18n.js
@@ -0,0 +1,13 @@
+/**
+ * Created by .
+ * User: dan
+ * Date: Feb 8, 2011
+ * Time: 11:59:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var i18n = require("./../index").i18n;
+exports["load"]["it does stuff"] = function() {
+
+  
+}
